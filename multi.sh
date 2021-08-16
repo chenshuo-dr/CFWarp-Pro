736727482afb0d6ea8a6d3baf62d9e5956d8de8e
chenshuo-dr <68913746+chenshuo-dr@users.noreply.github.com>
2021-08-16 11:56:19 +0800

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -739,169 +739,7 @@ function reboot(){
 sudo reboot
 }
 
-function dns(){
-echo 'DNS=8.8.8.8 2001:4860:4860::8888'>> /etc/systemd/resolved.conf
-systemctl restart systemd-resolved
-systemctl enable systemd-resolved
-mv /etc/resolv.conf  /etc/resolv.conf.bak
-ln -s /run/systemd/resolve/resolv.conf /etc/
-sudo reboot
-}
 
-function status(){
-systemctl status wg-quick@wgcf
-}
-
-function up4(){
-wget -N --no-check-certificate https://raw.githubusercontent.com/YG-tsj/CFWarp-Pro/main/multi.sh && chmod +x multi.sh && ./multi.sh
-}
-
-function up6(){
-echo -e nameserver 2a00:1098:2c::1 > /etc/resolv.conf
-wget -6 -N --no-check-certificate https://raw.githubusercontent.com/YG-tsj/CFWarp-Pro/main/multi.sh && chmod +x multi.sh && ./multi.sh
-}
-
-#主菜单
-function start_menu(){
-    clear
-    yellow " 详细说明 https://github.com/YG-tsj/CFWarp-Pro  YouTube频道：甬哥侃侃侃" 
-    
-    red " 切记：进入脚本快捷方式 bash multi.sh "
-    
-    white " ==================一、VPS相关调整选择（更新中）==========================================" 
-    
-    green " 1. 永久开启甲骨文VPS的ubuntu系统所有端口 "
-    
-    green " 2. 更新系统内核 "
-    
-    green " 3. 开启原生BBR加速 "
-    
-    green " 4. 检测奈飞Netflix是否解锁 "
-    
-    white " ==================二、“内核集成模式”WARP功能选择（更新中）======================================"
-    
-    yellow " ----VPS原生IP数------------------------------------添加WARP虚拟IP的位置--------------"
-    
-    green " 5. 纯IPV4的VPS。                                   添加WARP虚拟IPV6               "
-    
-    green " 6. 纯IPV4的VPS。                                   添加WARP虚拟IPV4+虚拟IPV6      "
-    
-    green " 7. 纯IPV4的VPS。                                   添加WARP虚拟IPV4              "
-    
-    green " 8. 双栈IPV4+IPV6的VPS。                            添加WARP虚拟IPV6               "
-    
-    green " 9. 双栈IPV4+IPV6的VPS。                            添加WARP虚拟IPV4+虚拟IPV6      "
-    
-    green " 10. 双栈IPV4+IPV6的VPS。                           添加WARP虚拟IPV4               "
-    
-    green " 11. 纯IPV6的VPS。                                  添加WARP虚拟IPV6               "
-    
-    green " 12. 纯IPV6的VPS。                                  添加WARP虚拟IPV4+虚拟IPV6       "
-    
-    green " 13. 纯IPV6的VPS。                                  添加WARP虚拟IPV4               "
-    
-    white " ------------------------------------------------------------------------------------------------"
-    
-    green " 14. 统一DNS功能 "
-    
-    green " 15. 永久关闭WARP功能 "
-    
-    green " 16. 自动开启WARP功能 "
-    
-    green " 17. 有IPV4：更新脚本 "
-    
-    green " 18. 无IPV4：更新脚本 "
-    
-    white " ==================三、代理协议脚本选择（更新中）==========================================="
-    
-    green " 19.使用mack-a脚本（支持Xray, V2ray） "
-    
-    green " 20.使用phlinhng脚本（支持Xray, Trojan-go, SS+v2ray-plugin） "
-    
-    white " ============================================================================================="
-    
-    green " 21. 重启VPS实例，请重新连接SSH "
-    
-    white " ===============================================================================================" 
-    
-    green " 0. 退出脚本 "
-    Print_ALL_Status_menu
-    echo
-    read -p "请输入数字:" menuNumberInput
-    case "$menuNumberInput" in
-        1 )
-           iptables
-	;;
-	2 )
-           upcore
-	;;
-        3 )
-           BBR
-	;;
-	4 )
-           Netflix
-	;;    
-        5 )
-           warp6
-	;;
-        6 )
-           warp64
-	;;
-        7 )
-           warp4
-	;;
-        8 )
-           warp466
-	;;
-        9 )
-           warp4646
-	;;
-	10 )
-           warp464
-	;;
-	11 )
-           wo66
-	;;
-	12 )
-           wo646
-	;;
-	13 )
-           wo64
-	;;
-	14 )
-           dns
-	;;
-	15 )
-           cwarp
-	;;
-	16 )
-           owarp
-	;;
-	17 )
-           up4
-	;;
-	18 )
-           up6
-	;;
-	19 )
-           macka
-	;;
-	20 )
-           phlinhng
-	;;
-	21 )
-           reboot
-	;;
-        0 )
-            exit 1
-        ;;
-    esac
-}
-
-
-start_menu "first"  
-
-elif [[ ${bit} == "aarch64" ]]; then
 
 function warp6(){
 if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then 
@@ -946,14 +784,6 @@ function macka(){
 wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
 }
 
-function phlinhng(){
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -F
-curl -fsSL https://raw.staticdn.net/phlinhng/v2ray-tcp-tls-web/main/src/xwall.sh -o ~/xwall.sh && bash ~/xwall.sh
-}
-
 function cv46(){
         yellow "开始检测IPV4地址"
 	v4=`wget -qO- -4 ip.gs`
@@ -979,176 +809,12 @@ function reboot(){
 sudo reboot
 }
 
-function dns(){
-echo 'DNS=8.8.8.8 2001:4860:4860::8888'>> /etc/systemd/resolved.conf
-systemctl restart systemd-resolved
-systemctl enable systemd-resolved
-mv /etc/resolv.conf  /etc/resolv.conf.bak
-ln -s /run/systemd/resolve/resolv.conf /etc/
-sudo reboot
-}
-
-function arm5.11(){
-
-function v46(){
-cd /tmp
-wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/arm64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_arm64.deb
-wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/arm64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_arm64.deb 
-wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/arm64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_arm64.deb
-sudo dpkg -i *.deb
-sudo apt -f install -y
-sudo reboot
-}
-
-function v6(){
-echo -e nameserver 2a00:1098:2c::1 > /etc/resolv.conf
-cd /tmp
-wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/arm64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_arm64.deb
-wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/arm64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_arm64.deb 
-wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/arm64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_arm64.deb
-sudo dpkg -i *.deb
-sudo apt -f install -y
-sudo reboot
-}
-
-function menu(){
-    clear
-    green " 请确认当前的VPS属于以下哪种IP表现形式！" 
-    blue " 1. 纯IPV4/双栈IPV4+IPV6 "    
-    blue " 2. 纯IPV6 "
-    red " 0. 返回上一层 "
-    echo
-    read -p "请输入数字:" menuNumberInput
-    case "$menuNumberInput" in   
-     1 )
-        v46
-     ;;
-     2 )
-        v6
-     ;;
-     0 )
-       start_menu
-     ;;
-      esac
-}
-
-menu  
-}
-
-function status(){
-systemctl status wg-quick@wgcf
-}
-
-function up4(){
-wget -N --no-check-certificate https://raw.githubusercontent.com/YG-tsj/CFWarp-Pro/main/multi.sh && chmod +x multi.sh && ./multi.sh
-}
-
-function up6(){
-echo -e nameserver 2a00:1098:2c::1 > /etc/resolv.conf
-wget -6 -N --no-check-certificate https://raw.githubusercontent.com/YG-tsj/CFWarp-Pro/main/multi.sh && chmod +x multi.sh && ./multi.sh
-}
-
-function wro646(){
-yellow " 检测系统内核版本是否大于5.6版本 "
-if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then 
-	red " 检测到内核版本小于5.6，回到菜单，选择2，自动更新内核吧"
-	exit 1
-fi
-apt update
-apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
-wget -N -6 https://cdn.jsdelivr.net/gh/YG-tsj/CFWarp-Pro/wgcf
-cp wgcf /usr/local/bin/wgcf
-sudo chmod +x /usr/local/bin/wgcf
-echo | wgcf register
-until [ $? -eq 0 ]
-do
-sleep 1s
-echo | wgcf register
-done
-wgcf generate
-sed -i "5 s/^/PostUp = ip -6 rule add from $rv6 table main\n/" wgcf-profile.conf
-sed -i "6 s/^/PostDown = ip -6 rule delete from $rv6 table main\n/" wgcf-profile.conf
-sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
-sed -i 's/1.1.1.1/8.8.8.8,2001:4860:4860::8888/g' wgcf-profile.conf
-cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
-cp wgcf-profile.conf /etc/wireguard/wgcf.conf
-systemctl enable wg-quick@wgcf
-systemctl start wg-quick@wgcf
-rm -f wgcf*
-grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
-yellow " 检测是否成功启动（IPV4+IPV6）双栈Warp！\n 显示IPV4地址：$(wget -qO- -4 ip.gs) 显示IPV6地址：$(wget -qO- -6 ip.gs) "
-green " 如上方显示IPV4地址：8.…………，IPV6地址：2a09:…………，则说明成功啦！\n 如上方IPV4无IP显示,IPV6显示本地IP,则说明失败喽！！ "
-}
-
-
-
-function wro66(){
-yellow " 检测系统内核版本是否大于5.6版本 "
-if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then 
-	red " 检测到内核版本小于5.6，回到菜单，选择2，自动更新内核吧"
-	exit 1
-fi
-apt update
-apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
-wget -N -6 https://cdn.jsdelivr.net/gh/YG-tsj/CFWarp-Pro/wgcf
-cp wgcf /usr/local/bin/wgcf
-sudo chmod +x /usr/local/bin/wgcf
-echo | wgcf register
-until [ $? -eq 0 ]
-do
-sleep 1s
-echo | wgcf register
-done
-wgcf generate
-sed -i "5 s/^/PostUp = ip -6 rule add from $rv6 table main\n/" wgcf-profile.conf
-sed -i "6 s/^/PostDown = ip -6 rule delete from $rv6 table main\n/" wgcf-profile.conf
-sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
-sed -i '/0\.0\.0\.0\/0/d' wgcf-profile.conf
-sed -i 's/1.1.1.1/2001:4860:4860::8888,8.8.8.8/g' wgcf-profile.conf
-cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
-cp wgcf-profile.conf /etc/wireguard/wgcf.conf
-systemctl enable wg-quick@wgcf
-systemctl start wg-quick@wgcf
-rm -f wgcf*
-yellow " 检测是否成功启动Warp！\n 显示IPV6地址：$(wget -qO- -6 ip.gs) "
-green " 如上方显示IPV6地址：2a09:…………，则说明成功！\n 如上方无IP显示，则说明失败喽！ "
-}
-
-
-function wro64(){
-yellow " 检测系统内核版本是否大于5.6版本 "
-if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then 
-	red " 检测到内核版本小于5.6，回到菜单，选择2，自动更新内核吧"
-	exit 1
-fi
-apt update
-apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
-wget -N -6 https://cdn.jsdelivr.net/gh/YG-tsj/CFWarp-Pro/wgcf
-cp wgcf /usr/local/bin/wgcf
-sudo chmod +x /usr/local/bin/wgcf
-echo | wgcf register
-until [ $? -eq 0 ]
-do
-sleep 1s
-echo | wgcf register
-done
-wgcf generate
-sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
-sed -i '/\:\:\/0/d' wgcf-profile.conf
-sed -i 's/1.1.1.1/8.8.8.8,2001:4860:4860::8888/g' wgcf-profile.conf
-cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
-cp wgcf-profile.conf /etc/wireguard/wgcf.conf
-systemctl enable wg-quick@wgcf
-systemctl start wg-quick@wgcf
-rm -f wgcf*
-grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
-yellow " 检测是否成功启动Warp！\n 显示IPV4地址：$(wget -qO- -4 ip.gs) "
-green " 如上方显示IPV4地址：8.…………，则说明成功啦！\n 如上方显示VPS本地IP,则说明失败喽！ "
-}
-
 
 #主菜单
 function start_menu(){
+
+    apt-get update -y
+    apt-get install curl wget -y
     
     green " 1. 更新系统内核 "
     
@@ -1168,71 +834,29 @@ function start_menu(){
     read -p "请输入数字:" menuNumberInput
     case "$menuNumberInput" in
 
-	2 )
-           arm5.11
+	1 )
+           upcore
 	;;
-        3 )
+        2 )
            BBR
 	;;
-	4 )
+	3 )
            Netflix
 	;;    
-        5 )
+        4 )
            warp6
 	;;
-        6 )
-           warp64
-	;;
-        7 )
-           warp4
-	;;
-        8 )
-           warp466
-	;;
-        9 )
-           warp4646
-	;;
-	10 )
-           warp464
-	;;
-	11 )
-           wro66
-	;;
-	12 )
-           wro646
-	;;
-	13 )
-           wro64
-	;;
-	14 )
-           dns
-	;;
-	15 )
-           cwarp
-	;;
-	16 )
-           owarp
-	;;
-	17 )
-           up4
-	;;
-	18 )
-           up6
-	;;
-	19 )
+        5 )
            macka
 	;;
-	20 )
+        6 )
            reboot
 	;;
-        0 )
-            exit 1
-        ;;
+        7 )
+	  exit 1
+	;;
     esac
 }
-
-
-start_menu "first"  
 
 else
  yellow "此CPU架构不是X86,也不是ARM！"
